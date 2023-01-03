b562d644c7e0279701fc95a1acd047d7bb682ea1
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-01-03 23:22:14 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string Reverse(string text)
+    {
+        return String.Join(" ", text.Split(' ').Reverse());
+    }
+}
